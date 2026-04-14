ba21e7f13e2a23040005b23e4c04bf7652e8681e
Luke Jackson <lj00021@mix.wvu.edu>
2026-04-14 17:54:49 -0400

    Same, just with the key columns that are going to be used, setting constant, used dict to have table: [column, column],

--- src/wvu_ieng_331_m2_2/validation.py
+++ src/wvu_ieng_331_m2_2/validation.py
@@ -20,3 +20,11 @@ EXPECTED_TABLES: set[str] = {
     "products",
     "sellers",
 }
+
+KEY_COLUMNS: dict[str, list[str]] = {
+    "orders": ["order_id", "customer_id"],
+    "order_items": ["order_id", "product_id", "seller_id"],
+    "customers": ["customer_id"],
+    "products": ["product_id"],
+    "sellers": ["seller_id"],
+}
